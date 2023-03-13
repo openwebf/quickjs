15af6c316bc766fb31ebde26e2929a5428943d07
ErosZy <zyeros1991@gmail.com>
2023-03-13 11:43:30 +0800

    fix: add test specs and free atom when ic retain it

--- src/core/function.c
+++ src/core/function.c
@@ -1489,7 +1489,8 @@ restart:
           ic->updated = FALSE;
           put_u8(pc - 5, OP_get_field_ic);
           put_u32(pc - 4, ic->updated_offset);
-          JS_FreeAtom(ctx, atom);
+          // safe free call because ic struct will retain atom
+          JS_FreeAtom(ic->ctx, atom);
         }
         JS_FreeValue(ctx, sp[-1]);
         sp[-1] = val;
@@ -1526,7 +1527,8 @@ restart:
           ic->updated = FALSE;
           put_u8(pc - 5, OP_get_field2_ic);
           put_u32(pc - 4, ic->updated_offset);
-          JS_FreeAtom(ctx, atom);
+          // safe free call because ic struct will retain atom
+          JS_FreeAtom(ic->ctx, atom);
         }
         *sp++ = val;
       }
@@ -1563,7 +1565,8 @@ restart:
           ic->updated = FALSE;
           put_u8(pc - 5, OP_put_field_ic);
           put_u32(pc - 4, ic->updated_offset);
-          JS_FreeAtom(ctx, atom);
+          // safe free call because ic struct will retain atom
+          JS_FreeAtom(ic->ctx, atom);
         }
       }
       BREAK;
